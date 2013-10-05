2e77911df4abf3392117e2ab9b83abf93e7bcd8d
Richard Russon (flatcap) <richard.russon@gmail.com>
2013-10-05 14:24:45 +0100

    select gui

--- plugman.c
+++ plugman.c
@@ -1,71 +1,87 @@
 #include <stdlib.h>
 #include <gtk/gtk.h>
 
+int state_button1 = -1;
+int state_button2 = -1;
+int state_button3 = -1;
+int state_button4 = -1;
+int state_button5 = -1;
+int state_button6 = -1;
+int state_button7 = -1;
+
+GtkWidget *b1, *b2, *b3, *b4, *b5, *b6, *b7;
+GtkWidget *x1, *x2, *x3, *x4, *x5, *x6, *x7;
+GtkWidget *c1, *c2, *c3, *c4, *c5, *c6, *c7;
+
+GdkRGBA red   = { 1.0, 0.0, 0.0, 1.0 };
+GdkRGBA green = { 0.0, 1.0, 0.0, 1.0 };
+
+static void set_colours()
+{
+	GdkRGBA *col1 = &red;
+	GdkRGBA *col2 = &red;
+	GdkRGBA *col3 = &red;
+	GdkRGBA *col4 = &red;
+	GdkRGBA *col5 = &red;
+	GdkRGBA *col6 = &red;
+	GdkRGBA *col7 = &red;
+
+	if (state_button1 == 1)
+		col1 = &green;
+
+	gtk_color_chooser_set_rgba ((GtkColorChooser*) c1, col1);
+	gtk_color_chooser_set_rgba ((GtkColorChooser*) c2, col2);
+	gtk_color_chooser_set_rgba ((GtkColorChooser*) c3, col3);
+	gtk_color_chooser_set_rgba ((GtkColorChooser*) c4, col4);
+	gtk_color_chooser_set_rgba ((GtkColorChooser*) c5, col5);
+	gtk_color_chooser_set_rgba ((GtkColorChooser*) c6, col6);
+	gtk_color_chooser_set_rgba ((GtkColorChooser*) c7, col7);
+}
+
 /**
- * activate_toggle
+ * set_tristate
+ */
+static void set_tristate (GtkWidget *check, int value)
+{
+	GtkToggleButton *t = (GtkToggleButton*) check;
+
+	switch (value) {
+		case -1:
+			gtk_toggle_button_set_inconsistent (t, TRUE);
+			break;
+		case 0:
+			gtk_toggle_button_set_inconsistent (t, FALSE);
+			gtk_toggle_button_set_active       (t, FALSE);
+			break;
+		case 1:
+			gtk_toggle_button_set_inconsistent (t, FALSE);
+			gtk_toggle_button_set_active       (t, TRUE);
+			break;
+	}
+	set_colours();
+}
+
+/**
+ * button_press
  */
 static void
-activate_toggle (GSimpleAction *action, GVariant *parameter, gpointer user_data)
+button_press (GtkWidget *button, void *param)
 {
-	GVariant *state;
-
-	state = g_action_get_state (G_ACTION (action));
-	g_action_change_state (G_ACTION (action), g_variant_new_boolean (!g_variant_get_boolean (state)));
-	g_variant_unref (state);
+	if (button == b1) {			// DEFAULT
+		if (state_button1 == 1)
+			state_button1 = 0;
+		else
+			state_button1 = 1;
+		set_tristate (x1, state_button1);
+	}
+	if (button == b2) printf ("b2\n");
+	if (button == b3) printf ("b3\n");
+	if (button == b4) printf ("b4\n");
+	if (button == b5) printf ("b5\n");
+	if (button == b6) printf ("b6\n");
+	if (button == b7) printf ("b7\n");
 }
 
-/**
- * change_fullscreen_state
- */
-static void
-change_fullscreen_state (GSimpleAction *action, GVariant *state, gpointer user_data)
-{
-	g_print ("fullscreen %d\n", g_variant_get_boolean (state));
-
-	g_simple_action_set_state (action, state);
-}
-
-/**
- * get_clipboard
- */
-static GtkClipboard *
-get_clipboard (GtkWidget *widget)
-{
-	return gtk_widget_get_clipboard (widget, gdk_atom_intern_static_string ("CLIPBOARD"));
-}
-
-/**
- * window_copy
- */
-static void
-window_copy (GSimpleAction *action, GVariant *parameter, gpointer user_data)
-{
-	GtkWindow *window = GTK_WINDOW (user_data);
-	GtkTextView *text = g_object_get_data ((GObject*)window, "plugman-text");
-
-	gtk_text_buffer_copy_clipboard (gtk_text_view_get_buffer (text), get_clipboard ((GtkWidget*) text));
-}
-
-/**
- * window_paste
- */
-static void
-window_paste (GSimpleAction *action, GVariant *parameter, gpointer user_data)
-{
-	GtkWindow *window = GTK_WINDOW (user_data);
-	GtkTextView *text = g_object_get_data ((GObject*)window, "plugman-text");
-
-	gtk_text_buffer_paste_clipboard (gtk_text_view_get_buffer (text), get_clipboard ((GtkWidget*) text), NULL, TRUE);
-}
-
-/**
- * GActionEntry win_entries[]
- */
-static GActionEntry win_entries[] = {
-	{ "copy", window_copy, NULL, NULL, NULL },
-	{ "paste", window_paste, NULL, NULL, NULL },
-	{ "fullscreen", activate_toggle, NULL, "false", change_fullscreen_state }
-};
 
 /**
  * new_window
@@ -77,7 +93,6 @@ new_window (GApplication *app, GFile *file)
 
 	window = gtk_application_window_new (GTK_APPLICATION (app));
 	gtk_window_set_default_size ((GtkWindow*)window, 640, 480);
-	g_action_map_add_action_entries (G_ACTION_MAP (window), win_entries, G_N_ELEMENTS (win_entries), window);
 	gtk_window_set_title (GTK_WINDOW (window), "Plugman");
 
 	grid = gtk_grid_new ();
@@ -147,182 +162,7 @@ plug_man_finalize (GObject *object)
 	G_OBJECT_CLASS (plug_man_parent_class)->finalize (object);
 }
 
-/**
- * show_about
- */
-static void
-show_about (GSimpleAction *action, GVariant *parameter, gpointer user_data)
-{
-	g_print ("about\n");
-}
 
-/**
- * quit_app
- */
-static void
-quit_app (GSimpleAction *action, GVariant *parameter, gpointer user_data)
-{
-	GList *list, *next;
-	GtkWindow *win;
-
-	//g_print ("Going down...\n");
-
-	list = gtk_application_get_windows (GTK_APPLICATION (g_application_get_default ()));
-	while (list) {
-		win = list->data;
-		next = list->next;
-
-		gtk_widget_destroy (GTK_WIDGET (win));
-
-		list = next;
-	}
-}
-
-
-#if 0
-static gboolean is_red_plugin_enabled;
-static gboolean is_black_plugin_enabled;
-
-/**
- * plugin_enabled
- */
-static gboolean
-plugin_enabled (const gchar *name)
-{
-	if (g_strcmp0 (name, "red") == 0)
-		return is_red_plugin_enabled;
-	else
-		return is_black_plugin_enabled;
-}
-
-/**
- * find_plugin_menu
- */
-static GMenuModel *
-find_plugin_menu (void)
-{
-	return (GMenuModel*) g_object_get_data (G_OBJECT (g_application_get_default ()), "plugin-menu");
-}
-
-/**
- * plugin_action
- */
-static void
-plugin_action (GAction	*action, GVariant *parameter, gpointer	data)
-{
-	GApplication *app;
-	GList *list;
-	GtkWindow *window;
-	GtkWidget *text;
-	GdkRGBA color;
-
-	app = g_application_get_default ();
-	list = gtk_application_get_windows (GTK_APPLICATION (app));
-	window = GTK_WINDOW (list->data);
-	text = g_object_get_data ((GObject*)window, "plugman-text");
-
-	gdk_rgba_parse (&color, g_action_get_name (action));
-
-	gtk_widget_override_color (text, 0, &color);
-}
-
-/**
- * enable_plugin
- */
-static void
-enable_plugin (const gchar *name)
-{
-	GMenuModel *plugin_menu;
-	GAction *action;
-
-	g_print ("Enabling '%s' plugin\n", name);
-
-	action = (GAction *)g_simple_action_new (name, NULL);
-	g_signal_connect (action, "activate", G_CALLBACK (plugin_action), (gpointer)name);
-	g_action_map_add_action (G_ACTION_MAP (g_application_get_default ()), action);
-	g_print ("Actions of '%s' plugin added\n", name);
-	g_object_unref (action);
-
-	plugin_menu = find_plugin_menu ();
-	if (plugin_menu) {
-		GMenu *section;
-		GMenuItem *item;
-		gchar *label;
-		gchar *action_name;
-
-		section = g_menu_new ();
-		label = g_strdup_printf ("Turn text %s", name);
-		action_name = g_strconcat ("app.", name, NULL);
-		g_menu_insert (section, 0, label, action_name);
-		g_free (label);
-		g_free (action_name);
-		item = g_menu_item_new_section (NULL, (GMenuModel*)section);
-		g_menu_item_set_attribute (item, "id", "s", name);
-		g_menu_append_item (G_MENU (plugin_menu), item);
-		g_object_unref (item);
-		g_object_unref (section);
-		g_print ("Menus of '%s' plugin added\n", name);
-	}
-	else {
-		g_warning ("Plugin menu not found\n");
-	}
-
-	if (g_strcmp0 (name, "red") == 0) {
-		is_red_plugin_enabled = TRUE;
-	} else {
-		is_black_plugin_enabled = TRUE;
-	}
-}
-
-/**
- * disable_plugin
- */
-static void
-disable_plugin (const gchar *name)
-{
-	GMenuModel *plugin_menu;
-
-	g_print ("Disabling '%s' plugin\n", name);
-
-	plugin_menu = find_plugin_menu ();
-	if (plugin_menu) {
-		const gchar *id;
-		gint i;
-
-		for (i = 0; i < g_menu_model_get_n_items (plugin_menu); i++) {
-				if (g_menu_model_get_item_attribute (plugin_menu, i, "id", "s", &id) && g_strcmp0 (id, name) == 0) {
-					g_menu_remove (G_MENU (plugin_menu), i);
-					g_print ("Menus of '%s' plugin removed\n", name);
-				}
-		}
-	} else {
-		g_warning ("Plugin menu not found\n");
-	}
-
-	g_action_map_remove_action (G_ACTION_MAP (g_application_get_default ()), name);
-	g_print ("Actions of '%s' plugin removed\n", name);
-
-	if (g_strcmp0 (name, "red") == 0) {
-		is_red_plugin_enabled = FALSE;
-	} else {
-		is_black_plugin_enabled = FALSE;
-	}
-}
-
-/**
- * enable_or_disable_plugin
- */
-static void
-enable_or_disable_plugin (GtkToggleButton *button, const gchar *name)
-{
-	if (plugin_enabled (name)) {
-		disable_plugin (name);
-	} else {
-		enable_plugin (name);
-	}
-}
-
-#endif
 /**
  * configure_plugins
  */
@@ -335,7 +175,7 @@ configure_plugins (GSimpleAction *action, GVariant *parameter, gpointer user_dat
 	GError *error = NULL;
 
 	builder = gtk_builder_new ();
-	gtk_builder_add_from_file (builder, "test2.ui", &error);
+	gtk_builder_add_from_file (builder, "select.ui", &error);
 
 	if (error) {
 		g_warning ("%s", error->message);
@@ -344,6 +184,47 @@ configure_plugins (GSimpleAction *action, GVariant *parameter, gpointer user_dat
 	}
 
 	dialog = (GtkWidget *)gtk_builder_get_object (builder, "window1");
+
+	b1 = (GtkWidget *)gtk_builder_get_object (builder, "button1");
+	b2 = (GtkWidget *)gtk_builder_get_object (builder, "button2");
+	b3 = (GtkWidget *)gtk_builder_get_object (builder, "button3");
+	b4 = (GtkWidget *)gtk_builder_get_object (builder, "button4");
+	b5 = (GtkWidget *)gtk_builder_get_object (builder, "button5");
+	b6 = (GtkWidget *)gtk_builder_get_object (builder, "button6");
+	b7 = (GtkWidget *)gtk_builder_get_object (builder, "button7");
+
+	x1 = (GtkWidget *)gtk_builder_get_object (builder, "checkbutton1");
+	x2 = (GtkWidget *)gtk_builder_get_object (builder, "checkbutton2");
+	x3 = (GtkWidget *)gtk_builder_get_object (builder, "checkbutton3");
+	x4 = (GtkWidget *)gtk_builder_get_object (builder, "checkbutton4");
+	x5 = (GtkWidget *)gtk_builder_get_object (builder, "checkbutton5");
+	x6 = (GtkWidget *)gtk_builder_get_object (builder, "checkbutton6");
+	x7 = (GtkWidget *)gtk_builder_get_object (builder, "checkbutton7");
+
+	c1 = (GtkWidget *)gtk_builder_get_object (builder, "colorbutton1");
+	c2 = (GtkWidget *)gtk_builder_get_object (builder, "colorbutton2");
+	c3 = (GtkWidget *)gtk_builder_get_object (builder, "colorbutton3");
+	c4 = (GtkWidget *)gtk_builder_get_object (builder, "colorbutton4");
+	c5 = (GtkWidget *)gtk_builder_get_object (builder, "colorbutton5");
+	c6 = (GtkWidget *)gtk_builder_get_object (builder, "colorbutton6");
+	c7 = (GtkWidget *)gtk_builder_get_object (builder, "colorbutton7");
+
+	gtk_toggle_button_set_inconsistent ((GtkToggleButton*) x1, TRUE);
+	gtk_toggle_button_set_inconsistent ((GtkToggleButton*) x2, TRUE);
+	gtk_toggle_button_set_inconsistent ((GtkToggleButton*) x3, TRUE);
+	gtk_toggle_button_set_inconsistent ((GtkToggleButton*) x4, TRUE);
+	gtk_toggle_button_set_inconsistent ((GtkToggleButton*) x5, TRUE);
+	gtk_toggle_button_set_inconsistent ((GtkToggleButton*) x6, TRUE);
+	gtk_toggle_button_set_inconsistent ((GtkToggleButton*) x7, TRUE);
+
+	g_signal_connect (b1, "clicked", G_CALLBACK (button_press), NULL);
+	g_signal_connect (b2, "clicked", G_CALLBACK (button_press), NULL);
+	g_signal_connect (b3, "clicked", G_CALLBACK (button_press), NULL);
+	g_signal_connect (b4, "clicked", G_CALLBACK (button_press), NULL);
+	g_signal_connect (b5, "clicked", G_CALLBACK (button_press), NULL);
+	g_signal_connect (b6, "clicked", G_CALLBACK (button_press), NULL);
+	g_signal_connect (b7, "clicked", G_CALLBACK (button_press), NULL);
+
 	/*
 	dialog = (GtkWidget *)gtk_builder_get_object (builder, "plugin-dialog");
 	check = (GtkWidget *)gtk_builder_get_object (builder, "red-plugin");
@@ -363,8 +244,6 @@ configure_plugins (GSimpleAction *action, GVariant *parameter, gpointer user_dat
  * GActionEntry app_entries[]
  */
 static GActionEntry app_entries[] = {
-	{ "about", show_about, NULL, NULL, NULL },
-	{ "quit", quit_app, NULL, NULL, NULL },
 	{ "plugins", configure_plugins, NULL, NULL, NULL },
 };
 
@@ -384,51 +263,13 @@ plug_man_startup (GApplication *application)
 	builder = gtk_builder_new ();
 	gtk_builder_add_from_string (builder,
 		"<interface>"
-		"	<menu id='app-menu'>"
-		"		<section>"
-		"			<item>"
-		"				<attribute name='label' translatable='yes'>_About Plugman</attribute>"
-		"				<attribute name='action'>app.about</attribute>"
-		"			</item>"
-		"		</section>"
-		"		<section>"
-		"			<item>"
-		"				<attribute name='label' translatable='yes'>_Quit</attribute>"
-		"				<attribute name='action'>app.quit</attribute>"
-		"				<attribute name='accel'>&lt;Primary&gt;q</attribute>"
-		"			</item>"
-		"		</section>"
-		"	</menu>"
 		"	<menu id='menubar'>"
 		"		<submenu>"
 		"			<attribute name='label' translatable='yes'>_Edit</attribute>"
-		"			<section>"
-		"				<item>"
-		"					<attribute name='label' translatable='yes'>_Copy</attribute>"
-		"					<attribute name='action'>win.copy</attribute>"
-		"				</item>"
-		"				<item>"
-		"					<attribute name='label' translatable='yes'>_Paste</attribute>"
-		"					<attribute name='action'>win.paste</attribute>"
-		"				</item>"
-		"			</section>"
-		"			<item><link name='section' id='plugins'>"
-		"			</link></item>"
-		"			<section>"
-		"				<item>"
-		"					<attribute name='label' translatable='yes'>Plugins</attribute>"
-		"					<attribute name='action'>app.plugins</attribute>"
-		"				</item>"
-		"			</section>"
-		"		</submenu>"
-		"		<submenu>"
-		"			<attribute name='label' translatable='yes'>_View</attribute>"
-		"			<section>"
-		"				<item>"
-		"					<attribute name='label' translatable='yes'>_Fullscreen</attribute>"
-		"					<attribute name='action'>win.fullscreen</attribute>"
-		"				</item>"
-		"			</section>"
+		"			<item>"
+		"				<attribute name='label' translatable='yes'>Plugins</attribute>"
+		"				<attribute name='action'>app.plugins</attribute>"
+		"			</item>"
 		"		</submenu>"
 		"	</menu>"
 		"</interface>", -1, NULL);
@@ -469,7 +310,7 @@ plug_man_class_init (PlugManClass *class)
 PlugMan *
 plug_man_new (void)
 {
-	return g_object_new (plug_man_get_type (), "application-id", "org.gtk.Test.plugman", "flags", G_APPLICATION_HANDLES_OPEN, NULL);
+	return g_object_new (plug_man_get_type (), "application-id", "org.flatcap.test.plugman", "flags", G_APPLICATION_HANDLES_OPEN, NULL);
 }
 
 /**
@@ -482,7 +323,6 @@ main (int argc, char **argv)
 	int status;
 
 	plug_man = plug_man_new ();
-	gtk_application_add_accelerator (GTK_APPLICATION (plug_man), "F11", "win.fullscreen", NULL);
 	status = g_application_run (G_APPLICATION (plug_man), argc, argv);
 	g_object_unref (plug_man);
 
