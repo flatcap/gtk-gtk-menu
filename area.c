c86db744451956ee96ab5c0a427c3b9eafd72208
Richard Russon (flatcap) <richard.russon@gmail.com>
2013-10-06 22:42:56 +0100

    app menu

--- area.c
+++ area.c
@@ -9,6 +9,73 @@ G_DEFINE_TYPE (Area, area, GTK_TYPE_APPLICATION)
 static GtkWidget *window = NULL;
 
 /**
+ * app_menu_preferences_cb
+ */
+static void
+app_menu_preferences_cb (GSimpleAction *action, GVariant *parameter, gpointer user_data)
+{
+	printf ("%s\n", __func__);
+}
+
+/**
+ * app_menu_help_cb (GSimpleAction *action, GVariant *parameter, gpointer user_data)
+ */
+static void
+app_menu_help_cb (GSimpleAction *action, GVariant *parameter, gpointer user_data)
+{
+	printf ("%s\n", __func__);
+}
+
+/**
+ * app_menu_about_cb (GSimpleAction *action, GVariant *parameter, gpointer user_data)
+ */
+static void
+app_menu_about_cb (GSimpleAction *action, GVariant *parameter, gpointer user_data)
+{
+	printf ("%s\n", __func__);
+}
+
+/**
+ * app_menu_quit_cb (GSimpleAction *action, GVariant *parameter, gpointer user_data)
+ */
+static void
+app_menu_quit_cb (GSimpleAction *action, GVariant *parameter, gpointer user_data)
+{
+	printf ("%s\n", __func__);
+	GtkApplication *application = user_data;
+	GtkWindow *window;
+
+	window = gtk_application_get_active_window (application);
+	gtk_widget_destroy (GTK_WIDGET (window));
+}
+
+
+/**
+ * app_has_app_menu
+ */
+static int
+app_has_app_menu (void)
+{
+	GtkSettings *gtk_settings;
+	gboolean show_app_menu;
+	gboolean show_menubar;
+
+	/* We have three cases:
+	 * - GNOME 3: show-app-menu true, show-menubar false -> use the app menu
+	 * - Unity, OSX: show-app-menu and show-menubar true -> use the normal menu
+	 * - Other WM, Windows: show-app-menu and show-menubar false -> use the normal menu
+	 */
+	gtk_settings = gtk_settings_get_default();
+	g_object_get (G_OBJECT (gtk_settings),
+	              "gtk-shell-shows-app-menu", &show_app_menu,
+	              "gtk-shell-shows-menubar", &show_menubar,
+	              NULL);
+	/* also: gtk-application-prefer-dark-theme */
+
+	return show_app_menu && !show_menubar;
+}
+
+/**
  * area_activate
  */
 static void
@@ -25,7 +92,7 @@ area_activate (GApplication *app)
 		gtk_window_set_title (GTK_WINDOW (window), "area");
 
 #if 0
-		builder = gtk_builder_new ();
+		builder = gtk_builder_new();
 		gtk_builder_add_from_file (builder, "area.ui", &error);
 
 		vbox = (GtkWidget *)gtk_builder_get_object (builder, "vbox2");
@@ -35,10 +102,32 @@ area_activate (GApplication *app)
 #endif
 	}
 
-	if (window) {
-		gtk_widget_show_all (window);
-		gtk_window_present (GTK_WINDOW (window));
+	gtk_icon_theme_append_search_path (gtk_icon_theme_get_default(), "/home/flatcap/work/gtk-menu");
+
+	const GActionEntry app_menu_actions[] = {
+		{ "preferences", app_menu_preferences_cb,   NULL, NULL, NULL },
+		{ "help",        app_menu_help_cb,          NULL, NULL, NULL },
+		{ "about",       app_menu_about_cb,         NULL, NULL, NULL },
+		{ "quit",        app_menu_quit_cb,          NULL, NULL, NULL }
+	};
+
+	if (app_has_app_menu()) {
+		GtkBuilder *builder = gtk_builder_new();
+		GError *error = NULL;
+
+		g_action_map_add_action_entries (G_ACTION_MAP (app), app_menu_actions, G_N_ELEMENTS (app_menu_actions), app);
+
+		if (!gtk_builder_add_from_file (builder, "appmenu.ui", &error)) {
+			g_warning ("loading menu builder file: %s", error->message);
+			g_error_free (error);
+		} else {
+			gtk_application_set_app_menu (GTK_APPLICATION (app), G_MENU_MODEL (gtk_builder_get_object (builder, "appmenu")));
+			g_object_unref (builder);
+		}
 	}
+
+	gtk_widget_show_all (window);
+	gtk_window_present (GTK_WINDOW (window));
 }
 
 /**
@@ -55,10 +144,11 @@ area_finalize (GObject *object)
  * area_startup
  */
 static void
-area_startup (GApplication *application)
+area_startup (GApplication *app)
 {
 	printf ("Entering: %s\n", __func__);
-	G_APPLICATION_CLASS (area_parent_class)->startup (application);
+	G_APPLICATION_CLASS (area_parent_class)->startup (app);
+
 }
 
 /**
